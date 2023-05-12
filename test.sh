229c296fafabcec1ad6d10ece05f77ff404684a9
Barry Wasdell <barry@bwsd.dev>
2023-05-12 22:58:43 +0200

    Simplify test script

--- test.sh
+++ test.sh
@@ -1,29 +1,10 @@
 #!/bin/bash
-set -e
-set -o pipefail
+set -eo pipefail
+. ./bash/.env
 
-OPTS=${OPTS:-}
-if [ -n "$2" ]; then
-	OPTS="$2"
-	shift;
-fi
-
-errors=()
-srcs=$(git diff --cached --name-only --diff-filter=ACM HEAD | sort -u)
-
-for sh in $srcs; do
-	if file "$sh" | grep -qlE "POSIX shell|Bourne-Again shell script"; then
-		{
-			shellcheck "$OPTS" -f gcc "$sh"
-		} || {
-			errors+=("$sh")
-		}
-	fi
-done
-
-if [ ${#errors[@]} -gt 0 ]; then
-    for e in "${errors[@]}"; do
-        echo "$e" 1>&2
-    done
-	exit 1
-fi
+git diff --cached --name-only --diff-filter=ACM HEAD |
+	sort -u |
+	xargs file |
+	grep -E "POSIX shell|Bourne-Again shell script|*bash*" - |
+	cut -d ':' -f1 |
+	xargs shellcheck "$SHELLCHECK_OPTS" -f gcc
