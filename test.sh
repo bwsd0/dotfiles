da9729763c4dcc9b8315903b73c51b729cc5d3a9
Barry Wasdell <barry@bwsd.dev>
2023-07-25 19:56:47 +0200

    Fix issue for case where no changes are staged

--- test.sh
+++ test.sh
@@ -2,12 +2,17 @@
 set -eo pipefail
 . ./bash/.env
 
-git diff --cached --name-only --diff-filter=ACM HEAD |
-	sort -u |
-	xargs file |
-	grep -E "POSIX shell|Bourne-Again shell script|*bash*" - |
-	cut -d ':' -f1 |
-	xargs shellcheck -x "$SHELLCHECK_OPTS" -f gcc
+modified="$(git diff --cached --name-only --diff-filter=ACM HEAD)"
+if [ -z "$modified" ]; then
+  exit 0
+fi
+
+echo "$modified" |
+  sort -u  |
+  xargs file |
+  grep -E "POSIX shell|Bourne-Again shell script|*bash*" - |
+  cut -d ':' -f1 |
+  xargs shellcheck -x "$SHELLCHECK_OPTS" -f gcc
 
 (
   tmpdir="$(mktemp -d)"
